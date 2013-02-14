62ec943a09b65654f01fd6ac7c41ae324825704e
FrancoisLaptopDell <FrancoisLaptopDell@c125642e-ac5a-2b4c-a1a2-6168a5ce109c>
2013-02-14 02:37:47 +0000

    git-svn-id: svn://localhost/trelloreports/trunk@3 c125642e-ac5a-2b4c-a1a2-6168a5ce109c

--- TrelloReports/MainWindow.xaml.cs
+++ TrelloReports/MainWindow.xaml.cs
@@ -40,10 +40,34 @@ namespace TrelloReports
 			var chelloClient = new ChelloClient(cAuthKey, cUserToken);
 
 			//var boards = chello.Members.PinnedBoards("francoishill");
-			var boards = chelloClient.Boards.ForUser("francoishill");
+			/*var boards = chelloClient.Boards.ForUser("francoishill");
 			foreach (var b in boards)
-				listOfBoards.Add(new TrelloBoard(chelloClient, b));
+				listOfBoards.Add(new TrelloBoard(chelloClient, b));*/
+
+			var wadisoBoardID = "50fd4e14c953c6797e00526f";
+			var wadisoBoard = chelloClient.Boards.Single(wadisoBoardID);
+			/*var cardUpdateTypes = chelloClient.CardUpdates.ForBoard(wadisoBoardID, new TempTrelloCardupdatesArguments(1000))
+				.GroupBy(c => c.Type)
+				.Select(kv => kv.Key)
+				.OrderBy(s => s);
+			foreach (var cu in cardUpdateTypes)
+				Console.WriteLine(cu);*/
+
+			listOfBoards.Add(new TrelloBoard(chelloClient, wadisoBoard));
 		}
+	}
+
+	public static class Trello
+	{
+		public enum CardUpdateTypes
+		{
+			AddChecklistToCard, AddMemberToBoard, AddMemberToCard,
+			CommentCard, CreateBoard, CreateCard, CreateList,
+			MakeAdminOfBoard, MakeNormalMemberOfBoard,
+			MoveCardFromBoard, MoveCardToBoard, MoveListFromBoard,
+			RemoveChecklistFromCard, RemoveMemberFromCard,
+			UpdateBoard, UpdateCard, UpdateCheckItemStateOnCard, UpdateChecklist
+		};
 	}
 
 	public class TrelloBoard
@@ -51,6 +75,7 @@ namespace TrelloReports
 		private Chello.Core.ChelloClient _chelloClient;
 		private Chello.Core.Board _chelloBoard;
 
+		public string BoardID { get { return _chelloBoard.Id; } }
 		public string BoardName { get { return _chelloBoard.Name; } }
 
 		private List<TrelloCardUpdate> _boardactivities = null;
@@ -61,9 +86,22 @@ namespace TrelloReports
 				if (_boardactivities == null)
 				{
 					_boardactivities = new List<TrelloCardUpdate>();
-					var updates = _chelloClient.CardUpdates.ForBoard(_chelloBoard.Id, new TrelloCardupdatesArguments(1000));
-					foreach (var upd in updates)
+					var cardUpdatesOnly = _chelloClient.CardUpdates.ForBoard(BoardID, new TempTrelloCardupdatesArguments(1000))
+						.Where(c => string.Equals(c.Type, Trello.CardUpdateTypes.UpdateCard.ToString(), StringComparison.InvariantCultureIgnoreCase)
+									&& c.Data != null
+									&& c.Data.ListAfter != null);
+
+					continue here
+					var listAfterGroups = cardUpdatesOnly
+						.GroupBy(c => c.Data.ListAfter.Name)
+						.ToDictionary(grpCard => grpCard.Key, grpCard => grpCard.Select(obj => obj.Data.ListAfter));
+
+					foreach (var upd in cardUpdatesOnly)
 						_boardactivities.Add(new TrelloCardUpdate(this._chelloClient, upd));
+
+					/*var updates = _chelloClient.CardUpdates.ForBoard(_chelloBoard.Id, new TrelloCardupdatesArguments(1000));
+					foreach (var upd in updates)
+						_boardactivities.Add(new TrelloCardUpdate(this._chelloClient, upd));*/
 				}
 				return _boardactivities;
 			}
@@ -86,6 +124,7 @@ namespace TrelloReports
 			private Chello.Core.CardUpdateAction _cardUpdateAction;
 
 			public DateTime CardupdateDateString { get { return _cardUpdateAction.Date; } }
+			public string UpdateType { get { return _cardUpdateAction.Type; } }
 			public bool CardClosed { get { return _cardUpdateAction.Data.Card != null ? _cardUpdateAction.Data.Card.Closed : false; } }
 			public string CardName { get { return _cardUpdateAction.Data.Card != null ? _cardUpdateAction.Data.Card.Name : ""; } }
 			public string CardDescription { get { return _cardUpdateAction.Data.Card != null ? _cardUpdateAction.Data.Card.Desc : ""; } }
@@ -112,6 +151,15 @@ namespace TrelloReports
 			{
 				this.limit = limit;
 			}
+		}
+	}
+
+	public class TempTrelloCardupdatesArguments
+	{
+		public int limit { get; private set; }
+		public TempTrelloCardupdatesArguments(int limit)
+		{
+			this.limit = limit;
 		}
 	}
 }
